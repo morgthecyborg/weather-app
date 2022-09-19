344808cf27f2c3c4d720bcdfcc493c517c9651a3
morgthecyborg <sanchmor@yahoo.com>
2022-09-18 22:18:08 -0500

    main body container, background changes
    Moved main body container to center, added 2 side columns, created background changing functionality that will change background color based on user's searchinput timezone

--- src/script.js
+++ src/script.js
@@ -37,13 +37,18 @@ function showWeather(response) {
   console.log(response.data);
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#city-header").innerHTML = response.data.name;
-  document.querySelector("#description").innerhtml = response.data.weather[1].description;
+  document.querySelector("#description").innerhtml = response.data.weather[0].description;
+  document.querySelector("#precipitation").innerHTML = Math.round(response.data.main.pressure);
+  document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+ 
 }
 
 function searchCityInput(city) {
   let apiKey = "946eb03de98f13f75348afbef5cbdb30";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(showWeather);
+  console.log(testing);
 }
 
 function receieveSubmitInput(event) {
@@ -54,3 +59,19 @@ function receieveSubmitInput(event) {
 
 let inputCity = document.querySelector("#search-form");
 inputCity.addEventListener("submit", receieveSubmitInput);
+
+//Changes background color by time of day
+setInterval(change_background, 1000 * 60 * 60);
+
+function change_background() {
+  if (hours == 23 || hours < 20) {
+    document.body.className = "night";
+  } else if(hours == 20|| hours > 18){
+    document.body.className = "evening";
+  }else{
+    document.body.className = "daylight";
+  }
+  }
+  console.log("test");
+
+change_background();
