bc9fef98aaa1743b6b812cfcaa9cdb5695aa46ac
morgthecyborg <sanchmor@yahoo.com>
2022-10-03 22:22:47 -0500

    Variable fix for showWeather function

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ function formatDate(timestamp){
 
 function showWeather(response) {
   console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature")
+  let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city-header");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
@@ -32,10 +32,14 @@ function showWeather(response) {
   let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   descriptionElement = response.data.weather[0].description;
   humidityElement = response.data.main.humidity;
   windElement = Math.round(response.data.wind.speed);
-  dateElement = formatDate(response.data.dt * 1000 );
+  dateElement = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src", displayImage(response.data.weather[0].icon));
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
