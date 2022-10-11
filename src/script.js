3445e66adb05453a09190ba18b79261abb37c9ed
morgthecyborg <sanchmor@yahoo.com>
2022-10-11 03:57:48 -0500

    Fixed and cleaned code

--- src/script.js
+++ src/script.js
@@ -1,8 +1,3 @@
-//FUNCTIONALITY FIRST
-
-//Create celcius farenheight conversion
-//Forecast Next
-
 function formatDate(timestamp){
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -36,12 +31,11 @@ function formatDay(timestamp){
 
 function getForecast(coordinates){
   let apiKey = "946eb03de98f13f75348afbef5cbdb30";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiURL).then(showForecast);
 }
 
 function showWeather(response) {
-  console.log(response.data);
   let celsiusElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city-header");
   let descriptionElement = document.querySelector("#description");
@@ -73,7 +67,7 @@ function showWeather(response) {
 
 function searchCityInput(city) {
   let apiKey = "946eb03de98f13f75348afbef5cbdb30";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiURL).then(showWeather);
 
 }
@@ -87,44 +81,28 @@ function receieveSubmitInput(event) {
 let inputCity = document.querySelector("#search-form");
 inputCity.addEventListener("submit", receieveSubmitInput);
 
-let fahrenheit = document.querySelector("#fahrenheit");
-let celsius = document.querySelector("#celsius");
-fahrenheit.addEventListener("click", showFahrenheitTemperature);
-celsius.addEventListener("click", showCelsiusTemperature);
+// let fahrenheit = document.querySelector("#fahrenheit");
+// let celsius = document.querySelector("#celsius");
+// fahrenheit.addEventListener("click", showFahrenheitTemperature);
+// celsius.addEventListener("click", showCelsiusTemperature);
 
-function showFahrenheitTemperature(event){
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsius.classList.remove("active");
-  fahrenheit.classList.add("active");
-  let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
+// function showFahrenheitTemperature(event){
+//   event.preventDefault();
+//   let temperatureElement = document.querySelector("#temperature");
+//   celsius.classList.remove("active");
+//   fahrenheit.classList.add("active");
+//   let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
+//   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+// }
 
-function showCelsiusTemperature(event){
-  event.preventDefault();
-  celsius.classList.add("active");
-  fahrenheit.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemperature = null;
-
-// //Changes background color by time of day
-// setInterval(change_background, 1000 * 60 * 60);
-
-// function change_background() {
-//   if (hours == 23 || hours < 20) {
-//     document.body.className = "night";
-//   } else if(hours == 20|| hours > 18){
-//     document.body.className = "evening";
-//   }else{
-//     document.body.className = "daylight";
-//   }
-//   }
-//   console.log("test");
-
-// change_background();
+// function showCelsiusTemperature(event){
+//   event.preventDefault();
+//   celsius.classList.add("active");
+//   fahrenheit.classList.remove("active");
+//   let temperatureElement = document.querySelector("#temperature");
+//   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+// }
+// let celsiusTemperature = null;
 
 function displayWeatherIcon(iconId){
   let iconClass = "";
@@ -153,7 +131,7 @@ function displayWeatherIcon(iconId){
   } else if (iconId === `50d` || iconId === `50n`){
     iconClass = `fa-smog`;
   }
-  return "fa-solid " + iconClass + " fa-4x icon-cog";
+  return "fa-solid " + iconClass + " fa-4x";
 }
 
 function showForecast(response){
